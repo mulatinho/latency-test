f6939592226791ae7be530e327cca76e3f671040
Alexandre Mulatinho <alex@mulatinho.net>
2025-05-12 00:57:59 -0300

    increasing tests
    Signed-off-by: Alexandre Mulatinho <alex@mulatinho.net>

--- tests/mlt.h
+++ tests/mlt.h
@@ -36,7 +36,7 @@ struct timeval mlt_init_t, mlt_subinit_t, mlt_end_t, mlt_subend_t;
 
 #define mlt_start() do { \
 	gettimeofday(&mlt_init_t, NULL); \
-	fprintf(stdout, ":. Started test(s) on '%s' at %s", \
+	fprintf(stdout, ":. Started test(s) on '%s' at %s\n", \
 		__FILE__, ctime((const time_t *) &mlt_init_t.tv_sec)); \
 	mlt_rsuccess = 0; mlt_rtests = 0; mlt_result = 0; \
 	} while(0)
@@ -55,7 +55,7 @@ struct timeval mlt_init_t, mlt_subinit_t, mlt_end_t, mlt_subend_t;
 	} while(0)
 
 #define mlt_debug(res) \
-	printf("return %s in '%s' on function '%s()' line %d, test '%s'\n", \
+	printf("return %s in '%s' on function '%s()' line %d,\ntest '%s'\n", \
 	res ? "success" : "error  ", __FILE__, __func__, __LINE__, __STRING(res))
 
 #define mlt_time_end() do { \
@@ -71,6 +71,5 @@ struct timeval mlt_init_t, mlt_subinit_t, mlt_end_t, mlt_subend_t;
 
 #define mlt_time_init() gettimeofday(&mlt_subinit_t, NULL);
 
-#define mlt_streq(in, str) \
-	mlt_assert(strncmp(in, str, strlen(in)) == 0);
+#define mlt_streq(in, str) mlt_assert(strncmp(in, str, strlen(in)) == 0);
 #define mlt_strneq(in, str) mlt_assert(strncmp(in, str, strlen(in)));
